3e122bad6f268ebbf5f46d8a77a9c8dfa98b806a
antz22 <anthonyznj@gmail.com>
2023-05-08 20:41:19 -0400

    add scrolling and better left click

--- virtual_mouse.py
+++ virtual_mouse.py
@@ -21,6 +21,8 @@ PINKY_PIP = 18
 PINKY_TIP = 20
 
 capture = cv2.VideoCapture(0)
+capture.set(3, 640)
+capture.set(4, 480)
 
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(min_detection_confidence=0.5,
@@ -31,13 +33,17 @@ previousTime = 0
 currentTime = 0
 
 frame_clicks = 0
+frame_pinches = 0
+pinch_up = False
+pinch_down = False
 mouse_down = False
+prev_landmarks = []
 
 size_x, size_y = pyautogui.size()
 
 while capture.isOpened():
     ret, frame = capture.read()
-    frame = cv2.resize(frame, (1500, 1200))
+    frame = cv2.resize(frame, (2000, 1600))
     image = cv2.cvtColor(cv2.flip(frame, 1), cv2.COLOR_BGR2RGB)
 
     image.flags.writeable = False
@@ -53,34 +59,61 @@ while capture.isOpened():
         right_landmarks = results.multi_hand_landmarks[num_hands - 1].landmark
 
         position = [right_landmarks[INDEX_FINGER_TIP].x, right_landmarks[INDEX_FINGER_TIP].y]
-        pyautogui.moveTo(position[0]*(1.4)*size_x-0.2*size_x, position[1]*(1.4)*size_y-0.2*size_y)
+        pyautogui.moveTo(position[0]*(1.5)*size_x-0.25*size_x, position[1]*(1.6)*size_y-0.25*size_y)
 
         if num_hands > 1:
             left_landmarks = results.multi_hand_landmarks[0].landmark
-            if dist(left_landmarks[THUMB_TIP], left_landmarks[INDEX_FINGER_TIP]) < 0.035:
-                pyautogui.rightClick()
+            # if dist(left_landmarks[THUMB_TIP], left_landmarks[INDEX_FINGER_TIP]) < 0.035:
+            #     pyautogui.rightClick()
 
-        if dist(right_landmarks[MIDDLE_FINGER_TIP], right_landmarks[THUMB_TIP]) < 0.03:
+            if dist(left_landmarks[INDEX_FINGER_TIP], left_landmarks[THUMB_TIP]) < 0.04:
+                if frame_pinches >= 5:
+                    if pinch_up:
+                        pyautogui.scroll(120)
+                    elif pinch_down:
+                        pyautogui.scroll(-120)
+                        
+                elif frame_pinches < 2:
+
+                    if prev_landmarks[0].landmark[THUMB_TIP].y < left_landmarks[THUMB_TIP].y:
+                        pinch_down = True
+                        pinch_up = False
+                    else:
+                        pinch_up = True
+                        pinch_down = False
+
+                frame_pinches += 1
+
+            else:
+                frame_pinches = 0
+                pinch_up = False
+                pinch_down = False
+
+
+        if dist(right_landmarks[MIDDLE_FINGER_TIP], right_landmarks[THUMB_TIP]) < 0.03 and right_landmarks[MIDDLE_FINGER_TIP].y < right_landmarks[THUMB_TIP].y:
             frame_clicks += 1
             print(dist(right_landmarks[MIDDLE_FINGER_TIP], right_landmarks[THUMB_TIP]))
 
-            if frame_clicks > 2:
-                if not mouse_down:
-                    mouse_down = True
-                    pyautogui.mouseDown()
-                else:
-                    mouse_down = False
-                    frame_clicks = 0
-                    pyautogui.mouseUp()
-            elif mouse_down:
-                mouse_down = False
-                frame_clicks = 0
-                pyautogui.mouseUp()
-            else:
+            # if frame_clicks > 2:
+            #     if not mouse_down:
+            #         mouse_down = True
+            #         pyautogui.mouseDown()
+            #     else:
+            #         mouse_down = False
+            #         frame_clicks = 0
+            #         pyautogui.mouseUp()
+            # elif mouse_down:
+            #     mouse_down = False
+            #     frame_clicks = 0
+            #     pyautogui.mouseUp()
+            # else:
+            if frame_clicks == 1:
                 pyautogui.click()
 
         else:
             frame_clicks = 0
+
+        prev_landmarks = results.multi_hand_landmarks
 
     currentTime = time.time()
     fps = 1 / (currentTime - previousTime)
